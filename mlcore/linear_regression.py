a82fdd22874881eb5ca4575f4a3a34fdf503e216
sush4nt <sushant.kb.patil@gmail.com>
2025-06-25 00:38:03 +0530

    linting and ci changes

--- mlcore/linear_regression.py
+++ mlcore/linear_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class CustomLinearRegression:
     def __init__(self, learning_rate=0.01, n_iters=100, l1=False, l2=False, alpha=1):
         self.learning_rate = learning_rate
@@ -18,7 +19,7 @@ class CustomLinearRegression:
         for _ in range(self.n_iters):
             # (m,n) x (n,1) = (m,1)
             hypothesis = np.dot(X, self.weights) + self.bias
-            cost_mse = (1 / 2*n_rows) * np.sum(np.square((hypothesis - y)))
+            cost_mse = (1 / 2 * n_rows) * np.sum(np.square((hypothesis - y)))
             # apply regularization and calculate gradients
             if self.l1:
                 regularization_term = self.alpha * np.sum(np.abs(self.weights))
@@ -30,17 +31,19 @@ class CustomLinearRegression:
             cost = cost_mse + regularization_term
             common_dw = (1 / n_rows) * np.dot(X.T, (hypothesis - y))
             dW = (
-                common_dw + self.alpha/n_rows * self.alpha * np.sign(self.weights)
-            ) if self.l1 else (
-                common_dw + self.alpha/n_rows * self.weights
-            ) if self.l2 else (
-                common_dw
+                (common_dw + self.alpha / n_rows * self.alpha * np.sign(self.weights))
+                if self.l1
+                else (
+                    (common_dw + self.alpha / n_rows * self.weights)
+                    if self.l2
+                    else (common_dw)
+                )
             )
             dB = (1 / n_rows) * np.sum(hypothesis - y)
             # update weights and bias
             self.weights -= self.learning_rate * dW
             self.bias -= self.learning_rate * dB
-    
+
     def predict(self, X):
         # (m,n) x (n,1) = (m,1)
         return np.dot(X, self.weights) + self.bias
--- mlcore/logistic_regression.py
+++ mlcore/logistic_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class CustomLogisticRegression:
     def __init__(self, learning_rate=0.01, n_iters=1000, l1=False, l2=False, alpha=1):
         self.learning_rate = learning_rate
@@ -19,12 +20,18 @@ class CustomLogisticRegression:
             # (m,n) x (n,1) = (m,1)
             hypothesis = self.sigmoid(np.dot(X, self.weights) + self.bias)
             # logloss term
-            cost_logloss = -(1 / n_rows) * np.sum( y * np.log(hypothesis) + (1 - y) * np.log(1 - hypothesis) )
+            cost_logloss = -(1 / n_rows) * np.sum(
+                y * np.log(hypothesis) + (1 - y) * np.log(1 - hypothesis)
+            )
             # find regularization cost
             if self.l1:
-                regularization_term = (self.alpha / n_rows) * np.sum(np.abs(self.weights))
+                regularization_term = (self.alpha / n_rows) * np.sum(
+                    np.abs(self.weights)
+                )
             elif self.l2:
-                regularization_term = (self.alpha / 2 * n_rows) * np.sum(np.square(self.weights))
+                regularization_term = (self.alpha / 2 * n_rows) * np.sum(
+                    np.square(self.weights)
+                )
             else:
                 regularization_term = 0
             # calculate total cost
@@ -34,18 +41,21 @@ class CustomLogisticRegression:
             dZ = hypothesis - y
             # (m,n) x (m,1) = (m,1)
             common_dw = (1 / n_rows) * np.dot(X.T, dZ)
-            dW = ( ( common_dw + self.alpha/n_rows * np.sign(self.weights) ) 
-            ) if self.l1 else (
-                common_dw + self.alpha/n_rows * self.weights 
-            ) if self.l2 else (
-                common_dw
+            dW = (
+                ((common_dw + self.alpha / n_rows * np.sign(self.weights)))
+                if self.l1
+                else (
+                    (common_dw + self.alpha / n_rows * self.weights)
+                    if self.l2
+                    else (common_dw)
+                )
             )
             # scalar
             dB = (1 / n_rows) * np.sum(dZ)
             # update weights
             self.weights -= self.learning_rate * dW
             self.bias -= self.learning_rate * dB
-    
+
     def predict_proba(self, X):
         if self.weights is None or self.bias is None:
             _, n_cols = X.shape
@@ -53,10 +63,10 @@ class CustomLogisticRegression:
             self.bias = 0
         # (m,n) x (n,1) = (m,1)
         return self.sigmoid(np.dot(X, self.weights) + self.bias)
-    
-    def predict(self, X, threshold=0.5):    
+
+    def predict(self, X, threshold=0.5):
         probabilities = self.predict_proba(X)
         return (probabilities >= threshold).astype(int)
-    
+
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
