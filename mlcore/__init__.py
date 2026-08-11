759318997b49910c39872e56cb39da771f8dddaf
sush4nt <sushant.kb.patil@gmail.com>
2026-08-11 22:26:17 +0530

    formatting

--- mlcore/__init__.py
+++ mlcore/__init__.py
@@ -1,8 +1,7 @@
 __version__ = "0.1.0"
 
 from .adaboost import CustomAdaBoostClassifier
-from .decision_tree import (CustomDecisionTreeClassifier,
-                            CustomDecisionTreeRegressor)
+from .decision_tree import CustomDecisionTreeClassifier, CustomDecisionTreeRegressor
 from .gaussian_nb import CustomGaussianNB
 from .gradientboost import CustomGradientBoostingClassifier
 from .knn import CustomKNN
